fa6a69bcd818ad1dbcf576b4e29cd75089b864e6
Hamza28Salhi <hamza.salhi@esprit.tn>
2023-04-28 00:12:28 +0100

    qrcode/stat

--- src/edu/worshop/controllers/AffichageMatchesBackController.java
+++ src/edu/worshop/controllers/AffichageMatchesBackController.java
@@ -5,19 +5,31 @@
  */
 package edu.worshop.controllers;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import edu.workshop.services.Matches1CRUD;
 import edu.worshop.interfaces.MatchesCRUD;
 import edu.worshop.model.Matches;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -25,8 +37,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 
@@ -44,6 +61,9 @@ static int id;
 static Date date;
 static String nom,stade,score,nomEquipeId;
 static Matches E = new Matches();
+    private TextField lieutf;
+    @FXML
+    private ImageView code_qr;
     /**
      * Initializes the controller class.
      */
@@ -161,6 +181,100 @@ for (int i = 0; i < list2.size(); i++) {
         Logger.getLogger(AjoutMatchesBackController.class.getName()).log(Level.SEVERE, null, ex);
         //showAlert("Error loading");
     }
+    }
+
+    private void map(ActionEvent event) throws IOException {
+          Parent previousScene = FXMLLoader.load(getClass().getResource("/edu/worshop/gui/Map.fxml"));
+    Scene scene = new Scene(previousScene);
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    stage.setScene(scene);
+    stage.show();
+    
+    }
+      public void setlocal(String ad){
+        this.lieutf.setText(ad);
+    }
+
+    @FXML
+    private void QrCode(ActionEvent event) {
+        Matches p = affichageMatchesBackfx.getSelectionModel().getSelectedItem();
+      
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        String Information = "nom du match : "+p.getNom()+"\n"+"Date : "+p.getDate();
+        int width = 300;
+        int height = 300;
+        BufferedImage bufferedImage = null;
+         try{
+            BitMatrix byteMatrix = qrCodeWriter.encode(Information, BarcodeFormat.QR_CODE, width, height);
+            bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            bufferedImage.createGraphics();
+            
+            Graphics2D graphics = (Graphics2D) bufferedImage.getGraphics();
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, width, height);
+            graphics.setColor(Color.BLACK);
+            
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (byteMatrix.get(i, j)) {
+                        graphics.fillRect(i, j, 1, 1);
+                    }
+                }
+            }
+            
+            System.out.println("Success...");
+            
+            code_qr.setImage(SwingFXUtils.toFXImage(bufferedImage, null));
+            
+        } catch (WriterException ex) {
+        }
+        
+        
+    }
+
+    @FXML
+    private void stat(ActionEvent event) {
+          // Create a map to store the frequency of each type
+    Map<String, Integer> typeFrequency = new HashMap<>();
+
+    // Loop through the items in the TableView
+    for (Matches match : affichageMatchesBackfx.getItems()) {
+        String type = match.nomEquipeId.getNom();
+        if (typeFrequency.containsKey(type)) {
+            typeFrequency.put(type, typeFrequency.get(type) + 1);
+        } else {
+            typeFrequency.put(type, 1);
+        }
+    }
+    
+    // Create a PieChart data set
+    ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+    for (String type : typeFrequency.keySet()) {
+        int frequency = typeFrequency.get(type);
+        double percentage = (double) frequency / affichageMatchesBackfx.getItems().size() * 100;
+        String percentageText = String.format("%.2f%%", percentage);
+        PieChart.Data slice = new PieChart.Data(type + " " + percentageText, frequency);
+        pieChartData.add(slice);
+    }
+
+    // Create a PieChart with the data set
+    PieChart chart = new PieChart(pieChartData);
+
+    // Show percentage values in the chart's tooltip
+    for (final PieChart.Data data : chart.getData()) {
+        Tooltip tooltip = new Tooltip();
+        tooltip.setText(String.format("%.2f%%", (data.getPieValue() / affichageMatchesBackfx.getItems().size() * 200)));
+        Tooltip.install(data.getNode(), tooltip);
+    }
+
+    // Show the chart in a new window
+    Scene scene = new Scene(chart);
+    Stage stage = new Stage();
+    stage.setScene(scene);
+    stage.show();
+        
+        
     }
 
 
